f3b16ce353db72cee9a31b6f1029a88b5eaf092f
Vishwa1011 <imt_2022119@iiitm.ac.in>
2025-05-09 19:03:53 +0530

    Batman

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 text-center">
+      <h1 className="text-3xl font-bold">SoftSell Coming Soon!</h1>
+      <p>This is a test to see if Tailwind is working.</p>
+    </div>
+  )
+}
+export default App
